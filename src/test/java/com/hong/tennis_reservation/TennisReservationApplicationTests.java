fa1d179769bd90492f99cb3d5310225099949def
hongEya <qaz7q@naver.com>
2021-12-10 09:56:08 +0900

    첫 프로젝트 파일 업로드

---
+++ src/test/java/com/hong/tennis_reservation/TennisReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.tennis_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
